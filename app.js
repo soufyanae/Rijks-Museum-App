5346da7c5f8cc80afaf2279e5fa485d289d53c26
soufyanae <39983632+soufyanae@users.noreply.github.com>
2018-06-10 23:58:06 +0200

    Add files via upload
    Deze bestanden zijn gewijzigd op 10/06 en bevatten de afbeeldingen van het behalen en het niet behalen van de quiz

---
+++ app.js
@@ -0,0 +1,59 @@
+function populate() {
+    if(quiz.isEnded ()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+        
+        //show choices
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i< choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess ("btn" +i, choices[i]);
+        }
+        
+        showProgress(); 
+    }
+};
+
+function guess (id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+}
+
+function showProgress() {
+    var currentQuestionsNumber = quiz.questionsIndex +1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question" + currentQuestionsNumber + " of " +quiz.questions.length;
+    
+}
+
+function showScores(){
+    var gameOverHtml = "<h1>Result</h1>";
+    gameOverHtml += "<h2 id = 'score'> Your scores: " +quiz.score +"</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHtml;
+    var element = document.getElementById("picture1").style.visibility = "visible";
+    element.innerHTML = gameOverHtml;
+};
+
+var questions = [
+    new Question("Hoe oud ben jij?", ["A", "B", "C", "D"], "D"),
+    new Question("Wat studeer jij?", ["A", "B", "C", "D"], "A"),
+    new Question("Wie is rijker?", ["A", "B", "C", "D"], "D"),
+    new Question("Hoe ben je zo lelijk?", ["A", "B", "C", "D"], "C"),
+    new Question("Hoe oud ben ik?", ["A", "B", "C", "D"], "B")
+];
+
+var quiz = new Quiz(questions);
+
+
+populate();
+
+
